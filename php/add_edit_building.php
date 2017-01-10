a41f405328f78fd86c64a4c42a366464ff77acc3
Jackson <kjackson@us-bottomline.root.bottomline.com>
2017-01-09 20:33:37 -0500

    forms

--- php/add_edit_building.php
+++ php/add_edit_building.php
@@ -1,8 +1,9 @@
 <h1>Add/Edit Building</h1>
 
 <!--<form id="add_building" method="">-->
+<div class="form-container building-container">
   <div class="form-row">
-    <label>Building Name:</label>
+    <label>Building Name</label>
     <input type="text" name="buildingName" />
   </div>
   <div class="form-row">
@@ -15,14 +16,36 @@
     </select>
   </div>
   <div class="form-row">
-    <label>Address:</label>
-    <textarea readonly="readonly"></textarea>
+    <label>Address Line 1</label>
+    <input type="text" name="addressLine1" />
+  </div>
+  <div class="form-row">
+    <label>Address Line 2</label>
+    <input type="text" name="addressLine2" />
+  </div>
+  <div class="form-row">
+    <label>City</label>
+    <input type="text" name="city" />
+  </div>
+  <div class="form-row">
+    <label>State</label>
+    <select name="state">
+      <option value="">--Choose One--</option>
+      <option value="ri">Rhode Island</option>
+      <option value="ma">Massachusetts</option>
+      <option value="ct">Connecticut</option>
+    </select>
+  </div>
+  <div class="form-row">
+    <label>Zip</label>
+    <input type="text" name="zip" />
   </div>
   <div class="form-row">
     <label></label>
     <button class="btn btn-default" onclick="loadPage('building')">Cancel</button>
     <button class="btn btn-success" name="save">Save</button>
   </div>
+</div>
 <!--</form>-->
 
 
